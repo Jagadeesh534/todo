43635d1b9fd64a07fb9d12d8b3e173387997824c
Jagadeesh534 <kalugotlajagadeesh@gmail.com>
2024-02-08 20:14:44 +0530

    deleted earlier and done some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import "./App.css";
-import AddTask from "./app/components/AddTask";
 import AddTaskV2 from "./app/components/AddTaskV2";
-import ListTasks from "./app/components/ListTasks";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import ListTasksV2 from "./app/components/ListTasksV2";
@@ -13,10 +11,7 @@ function App() {
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <div className="container d-flex  justify-content-center flex-column ">
           <h3 className="text-center text-primary m-3">Welcome</h3>
-          {/* <>
-            <AddTask></AddTask>
-            <ListTasks></ListTasks>
-          </> */}
+         
           <>
 
           <AddTaskV2></AddTaskV2>
